fc85aca7672639c65bc42606eaeccea752a26836
zczhao_glb <fhqfndn_glb@gmail.com>
2019-08-29 09:33:29 +0800

    普通Java工程/控制台项目，入口函数

--- springboot-012-principle/src/main/java/zzc/springboot/listener/HelloCommandLineRunner.java
+++ springboot-012-principle/src/main/java/zzc/springboot/listener/HelloCommandLineRunner.java
@@ -6,8 +6,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class HelloCommandLineRunner implements CommandLineRunner {
 
+	/**
+	 * 普通Java工程/控制台项目，入口函数
+	 */
     @Override
     public void run(String... strings) throws Exception {
+    	// 在此方法内写实现代码，程序启动时自动调用
         System.out.println("CommandLineRunner.run...");
     }
 
